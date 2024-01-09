2935ed137999ff270e406fb913788f88373908cd
JesusKris <robertkrislaur.media@gmail.com>
2024-01-09 13:42:24 +0200

    Renamed the project to translationapi

---
+++ translationapi/src/main/java/com/kood/homework/translationapi/TranslationapiApplication.java
@@ -0,0 +1,13 @@
+package com.kood.homework.translationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslationapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranslationapiApplication.class, args);
+	}
+
+}
---
+++ translationapi/src/test/java/com/kood/homework/translationapi/TranslationapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kood.homework.translationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslationapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
