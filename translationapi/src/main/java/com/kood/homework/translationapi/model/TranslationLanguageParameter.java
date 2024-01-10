54556a27b6cb2313e36e306317f7df279f691a9d
JesusKris <robertkrislaur.media@gmail.com>
2024-01-11 00:31:44 +0200

    Added documentation, linted

--- translationapi/src/main/java/com/kood/homework/translationapi/model/TranslationLanguageParameter.java
+++ translationapi/src/main/java/com/kood/homework/translationapi/model/TranslationLanguageParameter.java
@@ -1,12 +1,29 @@
 package com.kood.homework.translationapi.model;
 
 
+/**
+ * Represents parameters for language-related requests.
+ * It includes the type of language group (source or target).
+ * 
+ * <p>
+ * Used for verification of the request query parameters.
+ * 
+ * <p>
+ * <strong>Author:</strong> JesusKris
+ * </p>
+ */
 public class TranslationLanguageParameter {
     private final String type;
 
+
+    /**
+     * Constructs a new TranslationLanguageParameter with the specified type.
+     *
+     * @param type The type of language group (source or target).
+     * @throws IllegalArgumentException If the language group type is invalid.
+     */
     public TranslationLanguageParameter(String type)
             throws IllegalArgumentException {
-            
 
         if (!isValidType(type)) {
             throw new IllegalArgumentException("Invalid language group type");
