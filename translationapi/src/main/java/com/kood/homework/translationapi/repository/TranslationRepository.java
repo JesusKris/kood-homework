89b7766eac0f86e0b016fd0a6777d116278822fd
JesusKris <robertkrislaur.media@gmail.com>
2024-01-11 00:22:47 +0200

    Added documentation, linting

--- translationapi/src/main/java/com/kood/homework/translationapi/repository/TranslationRepository.java
+++ translationapi/src/main/java/com/kood/homework/translationapi/repository/TranslationRepository.java
@@ -1,16 +1,45 @@
 package com.kood.homework.translationapi.repository;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.kood.homework.translationapi.entity.TranslationEntity;
 import java.util.List;
 
+
+/**
+ * Spring Data JPA repository interface for managing {@link TranslationEntity}
+ * entities.
+ * 
+ * <p>
+ * <strong>Author:</strong> JesusKris
+ * </p>
+ */
 public interface TranslationRepository extends JpaRepository<TranslationEntity, Long> {
+
     
-    // Save method
+    /**
+     * Save and flush a translation entity.
+     *
+     * @param translation The translation entity to be saved and flushed.
+     * @return The saved and flushed translation entity.
+     */
     TranslationEntity saveAndFlush(TranslationEntity translation);
 
-    // Custom method to get all translations with optional sorting
+
+    /**
+     * Retrieve all translations ordered by creation timestamp in descending order.
+     *
+     * @return A list of translation entities ordered by creation timestamp in
+     *         descending order.
+     */
     List<TranslationEntity> findAllByOrderByCreatedAtDesc();
+
+
+    /**
+     * Retrieve all translations ordered by creation timestamp in ascending order.
+     *
+     * @return A list of translation entities ordered by creation timestamp in
+     *         ascending order.
+     */
     List<TranslationEntity> findAllByOrderByCreatedAtAsc();
-    
 }
