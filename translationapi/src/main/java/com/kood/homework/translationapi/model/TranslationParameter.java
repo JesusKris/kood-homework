a3442e95bfd062392c7d3ca4a8f770eb8dd4ee0c
JesusKris <robertkrislaur.media@gmail.com>
2024-01-11 00:28:28 +0200

    Added documentation, linted

--- translationapi/src/main/java/com/kood/homework/translationapi/model/TranslationParameter.java
+++ translationapi/src/main/java/com/kood/homework/translationapi/model/TranslationParameter.java
@@ -1,13 +1,31 @@
 package com.kood.homework.translationapi.model;
 
+
+/**
+ * Represents parameters for a translation request.
+ * It includes the input text, source language, and target language.
+ *
+ * <p>Used for verification of the request query parameters.
+ * 
+ * <p>
+ * <strong>Author:</strong> JesusKris
+ * </p>
+ */
 public class TranslationParameter {
     private final String inputText;
     private final String sourceLanguage;
     private final String targetLanguage;
 
-    public TranslationParameter(String inputText, String sourceLanguage, String targetLanguage)
-            throws IllegalArgumentException {
 
+    /**
+     * Constructs a new TranslationParameter with the specified input text, source language, and target language.
+     *
+     * @param inputText      The text to be translated.
+     * @param sourceLanguage The language of the input text.
+     * @param targetLanguage The target language for translation.
+     * @throws IllegalArgumentException If the input or target_language is invalid.
+     */
+    public TranslationParameter(String inputText, String sourceLanguage, String targetLanguage) throws IllegalArgumentException {
         if (!isValidInputText(inputText) || !isValidLanguage(targetLanguage) || !isValidLanguage(sourceLanguage)) {
             throw new IllegalArgumentException("Invalid input or target_language");
         }
