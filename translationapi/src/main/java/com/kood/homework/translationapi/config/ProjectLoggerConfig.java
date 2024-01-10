17477029722c3b9c50dd6ba47789476fa87a3643
JesusKris <robertkrislaur.media@gmail.com>
2024-01-11 00:52:08 +0200

    Linted

--- translationapi/src/main/java/com/kood/homework/translationapi/config/ProjectLoggerConfig.java
+++ translationapi/src/main/java/com/kood/homework/translationapi/config/ProjectLoggerConfig.java
@@ -1,10 +1,12 @@
 package com.kood.homework.translationapi.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.kood.homework.translationapi.TranslationapiApplication;
 import com.kood.homework.translationapi.model.ProjectLogger;
 import com.kood.homework.translationapi.util.LogBackLogger;
+
 
 /**
  * Configuration class for creating the ProjectLogger bean.
