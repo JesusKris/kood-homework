797dd9bf27dd60ddf6304fbbe956bd062d14af88
JesusKris <robertkrislaur.media@gmail.com>
2024-01-10 15:20:50 +0200

    Added ApiResponse interface for polymorphism

---
+++ translationapi/src/main/java/com/kood/homework/translationapi/model/ApiResponse.java
@@ -0,0 +1,10 @@
+package com.kood.homework.translationapi.model;
+
+/**
+ * Marker interface for representing API responses.
+ * This interface is intended to be implemented by classes
+ * representing different types of API responses.
+ * 
+ * <p><strong>Author:</strong> JesusKris</p>
+ */
+public interface ApiResponse {}
