1b184ed545d358c82ae3a9405226085437e340df
JesusKris <robertkrislaur.media@gmail.com>
2024-01-10 23:20:02 +0200

    Removed unnecessary imports, linting, code quality

--- translationapi/src/main/java/com/kood/homework/translationapi/TranslationapiApplication.java
+++ translationapi/src/main/java/com/kood/homework/translationapi/TranslationapiApplication.java
@@ -1,15 +1,17 @@
 package com.kood.homework.translationapi;
 
+
 import java.lang.System.Logger.Level;
 import java.net.InetAddress;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
-import org.springframework.core.env.Environment;
 import com.kood.homework.translationapi.model.ProjectLogger;
 import io.micrometer.common.util.StringUtils;
+
 
 @SpringBootApplication
 public class TranslationapiApplication implements ApplicationListener<ApplicationReadyEvent> {
@@ -17,22 +19,28 @@ public class TranslationapiApplication implements ApplicationListener<Applicatio
     @Autowired
     private ProjectLogger logger;
 
-    @Autowired
-    private Environment environment;
+    @Value("${translation.api.deepl.key}")
+    private String deeplApiKey;
+
+    @Value("${translation.api.server.port}")
+    private String serverPort;
+
 
     public static void main(String[] args) {
         SpringApplication.run(TranslationapiApplication.class, args);
     }
 
+
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
-        checkDeeplApiKey();
+        validateDeeplApiKeyPresence();
         logger.log(Level.INFO, "Spring Boot Translation API running on "
-                + InetAddress.getLoopbackAddress().getHostAddress() + ":" + environment.getProperty("server.port"));
+                + InetAddress.getLoopbackAddress().getHostAddress() + ":" + serverPort);
     }
 
-    private void checkDeeplApiKey() {
-        if (StringUtils.isEmpty(environment.getProperty("deepl.api.key"))) {
+
+    private void validateDeeplApiKeyPresence() {
+        if (StringUtils.isEmpty(deeplApiKey)) {
             throw new RuntimeException("DeepL API key not set. Please set the 'deepl.api.key' environment variable.");
         }
     }
