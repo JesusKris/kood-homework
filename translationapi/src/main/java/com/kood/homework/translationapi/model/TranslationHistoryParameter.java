52eded9276996d59436624a71fb4ee7062fc69cc
JesusKris <robertkrislaur.media@gmail.com>
2024-01-11 00:35:13 +0200

    Added documentation, linted

--- translationapi/src/main/java/com/kood/homework/translationapi/model/TranslationHistoryParameter.java
+++ translationapi/src/main/java/com/kood/homework/translationapi/model/TranslationHistoryParameter.java
@@ -1,9 +1,27 @@
 package com.kood.homework.translationapi.model;
 
-public class TranslationHistoryParameter {
 
+/**
+ * Represents parameters for translation history requests.
+ * It includes the order parameter to specify the sorting order (asc or desc).
+ * 
+ * 
+ * <p>Used for verification of the request query parameters.
+ * 
+ * <p>
+ * <strong>Author:</strong> JesusKris
+ * </p>
+ */
+public class TranslationHistoryParameter {
     private final String order;
 
+
+    /**
+     * Constructs a new TranslationHistoryParameter with the specified order.
+     *
+     * @param order The sorting order (asc or desc).
+     * @throws IllegalArgumentException If the order parameter is invalid.
+     */
     public TranslationHistoryParameter(String order) throws IllegalArgumentException {
         if (!isValidOrder(order)) {
             throw new IllegalArgumentException("Invalid order parameter");
