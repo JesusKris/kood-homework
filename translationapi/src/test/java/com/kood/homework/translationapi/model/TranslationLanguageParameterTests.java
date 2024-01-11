48568f04addee1444221e282946adf9519ccd2fa
JesusKris <robertkrislaur.media@gmail.com>
2024-01-11 14:21:11 +0200

    Added TranslationLanguageParameter tests

---
+++ translationapi/src/test/java/com/kood/homework/translationapi/model/TranslationLanguageParameterTests.java
@@ -0,0 +1,39 @@
+package com.kood.homework.translationapi.model;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class TranslationLanguageParameterTests {
+
+    @Test
+    void validTypeSource() {
+        TranslationLanguageParameter parameter = new TranslationLanguageParameter("source");
+        assertEquals("source", parameter.getType());
+    }
+
+    @Test
+    void validTypeTarget() {
+        TranslationLanguageParameter parameter = new TranslationLanguageParameter("target");
+        assertEquals("target", parameter.getType());
+    }
+
+    @Test
+    void invalidTypeNull() {
+        assertThrows(IllegalArgumentException.class, () -> new TranslationLanguageParameter(null));
+    }
+
+    @Test
+    void invalidTypeEmpty() {
+        assertThrows(IllegalArgumentException.class, () -> new TranslationLanguageParameter(""));
+    }
+
+    @Test
+    void invalidTypeUnknownValue() {
+        assertThrows(IllegalArgumentException.class, () -> new TranslationLanguageParameter("random"));
+    }
+}
