e50ac82cb560d4f4e59a8c7a2b3bcf3a9febe58b
JesusKris <robertkrislaur.media@gmail.com>
2024-01-11 14:09:01 +0200

    Added TranslationHistoryParameter tests

---
+++ translationapi/src/test/java/com/kood/homework/translationapi/model/TranslationHistoryParameterTests.java
@@ -0,0 +1,39 @@
+package com.kood.homework.translationapi.model;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class TranslationHistoryParameterTests {
+
+    @Test
+    void validOrderAsc() {
+        TranslationHistoryParameter parameter = new TranslationHistoryParameter("asc");
+        assertEquals("asc", parameter.getOrder());
+    }
+
+    @Test
+    void validOrderDesc() {
+        TranslationHistoryParameter parameter = new TranslationHistoryParameter("desc");
+        assertEquals("desc", parameter.getOrder());
+    }
+
+    @Test
+    void invalidOrderNull() {
+        assertThrows(IllegalArgumentException.class, () -> new TranslationHistoryParameter(null));
+    }
+
+    @Test
+    void invalidOrderEmptyString() {
+        assertThrows(IllegalArgumentException.class, () -> new TranslationHistoryParameter(""));
+    }
+
+    @Test
+    void invalidOrderUnknownValue() {
+        assertThrows(IllegalArgumentException.class, () -> new TranslationHistoryParameter("random"));
+    }
+}
